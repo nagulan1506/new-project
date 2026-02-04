a12c302b22dd4c295a7bb5f814bba6baf0a647b4
nagulan <nagulans6524@gmail.com>
2026-02-04 18:13:52 +0530

    commit

--- server/index.js
+++ server/index.js
@@ -13,6 +13,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get('/', (req, res) => {
+    res.send('Password Reset API is running and healthy!');
+});
+
 mongoose.connect(process.env.MONGODB_URI)
     .then(() => console.log('Connected to MongoDB'))
     .catch(err => console.error('MongoDB connection error:', err));
